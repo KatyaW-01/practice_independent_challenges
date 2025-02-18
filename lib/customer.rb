8c6b79bc24a6d74bb735af2d024fd4ddd3acd560
Katya Weicht <katya.weicht@gmail.com>
2025-02-18 15:58:26 -0700

    Finish iteration two

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,7 +1,17 @@
 class Customer
-    attr_reader :name
-    def initialize(name,pets)
+    attr_reader :name, :id, :pets, :outstanding_balance
+    def initialize(name,id)
         @name = name
+        @id = id
+        @pets = []
+        @outstanding_balance = 0
+    end
 
+    def adopt(pet)
+        @pets << pet
+    end
+
+    def charge(pet_cost)
+        @outstanding_balance += pet_cost
     end
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -11,8 +11,35 @@ RSpec.describe Customer do
         it 'exists' do
             expect(@joel).to be_a(Customer)
         end
+
         it 'has a name' do
             expect(@joel.name).to eq("Joel")
+        end
+
+        it 'has an id' do
+            expect(@joel.id).to eq(2)
+        end
+    end
+    describe "#adopt pets" do
+        it 'has an empty array of pets' do
+            expect(@joel.pets).to eq([])
+        end
+
+        it 'can adopt pets' do
+            @joel.adopt(@samson)
+            @joel.adopt(@lucy)
+
+            expect(@joel.pets).to eq([@samson,@lucy])
+        end
+
+        it 'can collect charges for pets' do
+            expect(@joel.outstanding_balance).to eq(0)
+
+            @joel.charge(15)
+            @joel.charge(7)
+
+            expect(@joel.outstanding_balance).to eq(22)
+
         end
     end
 
