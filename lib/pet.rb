27e93f40bd2988bb085e696227b23922c735ae92
Katya Weicht <katya.weicht@gmail.com>
2025-02-18 15:38:01 -0700

    Finish iteration one

--- lib/pet.rb
+++ lib/pet.rb
@@ -1,5 +1,17 @@
 class Pet
+    attr_reader :name, :type, :age
     def initialize(pet_information)
-        
+        @name = pet_information[:name]
+        @type = pet_information[:type]
+        @age = pet_information[:age]
+        @fed = false
+    end
+
+    def fed?
+        @fed
+    end
+
+    def feed
+        @fed = true
     end
 end
--- spec/pet_spec.rb
+++ spec/pet_spec.rb
@@ -1,9 +1,31 @@
 require './lib/pet'
 
 RSpec.describe Pet do
-    it 'exists' do
-        samson = Pet.new({name: "Samson", type: :dog, age: 3}) 
+    before(:each) do
+        @samson = Pet.new({name: "Samson", type: :dog, age: 3,}) 
+    end
 
-        expect(samson).to be_a(Pet)
+    it 'exists' do
+        expect(@samson).to be_a(Pet)
+    end
+
+    it 'has a name' do
+        expect(@samson.name).to eq("Samson")
+    end
+
+    it 'has a type' do
+        expect(@samson.type).to eq(:dog)
+    end
+
+    it 'has an age' do
+        expect(@samson.age).to eq(3)
+    end
+
+    it 'can return true/false based on if it has been fed' do
+        expect(@samson.fed?).to eq(false)
+    end
+    it 'can feed a pet' do
+        @samson.feed
+        expect(@samson.fed?).to eq(true)
     end
 end
