da0b403083fc44f86f3e945bdb5fc68ba13ccc60
Katya Weicht <katya.weicht@gmail.com>
2025-02-19 00:23:55 -0700

    Finish iteration three

--- lib/groomer.rb
+++ lib/groomer.rb
@@ -1,11 +1,31 @@
+require 'pry'
 class Groomer
-    attr_reader :customers
+    attr_reader :name, :customers, :customers_with_outstanding_balance
     def initialize(name)
+        @name = name
         @customers = []
+        @customers_with_outstanding_balance = []
+        
     end
 
     def add_customer(customer)
         @customers << customer
+    end
+
+    def find_outstanding_balances
+        @customers.find_all do |customer|
+            customer.outstanding_balance > 0
+            @customers_with_outstanding_balance << customer
+        end
+    end
+
+    def count_pet_type(pet_type)
+        pets = @customers.map do |customer|
+            customer.pets  
+        end
+        pets.flatten.count do |pet|
+           pet.type == pet_type
+        end
     end
 
 end
--- spec/groomer_spec.rb
+++ spec/groomer_spec.rb
@@ -12,6 +12,9 @@ RSpec.describe Groomer do
         it 'exists' do
             expect(@groomer).to be_a(Groomer)
         end
+        it 'has a name' do
+            expect(@groomer.name).to eq("Dog and Cat Groomer")
+        end
         it 'has customers' do
             expect(@groomer.customers).to eq([])
 
@@ -20,6 +23,41 @@ RSpec.describe Groomer do
 
             expect(@groomer.customers).to eq([@customer_1,@customer_2])
         end
+        it 'has customers with multiple pets' do
+            miso = Pet.new({name: "Miso", type: :cat, age: 1})
+            celery = Pet.new({name: "Celery", type: :cat, age: 1})
+
+            @customer_1.adopt(miso)
+            @customer_1.adopt(celery)
+
+            expect(@customer_1.pets).to eq([miso,celery])
+        end
+        it 'can find customers with outstanding balances' do
+            @groomer.add_customer(@customer_2)
+            @customer_2.charge(20)
+
+            expect(@groomer.find_outstanding_balances).to eq([@customer_2])
+        end
+
+        it 'can count the number of pets of a given type' do
+            @groomer.add_customer(@customer_1)
+            @groomer.add_customer(@customer_2)
+
+            miso = Pet.new({name: "Miso", type: :cat, age: 1})
+            celery = Pet.new({name: "Celery", type: :cat, age: 1})
+
+            zuko = Pet.new({name: "Zuko", type: :dog, age: 5})
+
+            @customer_1.adopt(miso)
+            @customer_1.adopt(celery)
+
+            @customer_2.adopt(zuko)
+
+            expect(@groomer.count_pet_type(:cat)).to eq(2)
+            expect(@groomer.count_pet_type(:dog)).to eq(1)
+
+        end
+        
     end
     
 end
